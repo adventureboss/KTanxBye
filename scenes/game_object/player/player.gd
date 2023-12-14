4f0225d5c4ace56cfecb9866a597bc9349f4de2f
Stephen <stephen.adams@redhat.com>
2023-12-14 12:35:37 -0500

    Add basic player scene and controls

---
+++ scenes/game_object/player/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 300.0
+const ACCELERATION_SMOOTHING = 25
+
+func _process(delta):
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	var target_velocity = direction * MAX_SPEED
+	
+	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
+	
+	move_and_slide()
+	
+	#var move_sign = sign(movement_vector.x)
+	#if move_sign != 0:
+		#visuals.scale = Vector2(move_sign, 1)
+
+
+func get_movement_vector():
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	return Vector2(x_movement, y_movement)
+	
