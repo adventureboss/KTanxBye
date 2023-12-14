45b495f07be4d5eada2df3e5e60916ef05e8d498
Stephen <stephen.adams@redhat.com>
2023-12-14 16:20:15 -0500

    Get the tank moving

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -3,14 +3,28 @@ extends CharacterBody2D
 const MAX_SPEED = 300.0
 const ACCELERATION_SMOOTHING = 25
 
+@onready var barrel_tip: Marker2D = $Barrel
+@onready var tank_body: Sprite2D = $TankBody/Sprite2D
+
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
 	var target_velocity = direction * MAX_SPEED
+	var mouse_position = get_global_mouse_position()
 	
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	
 	move_and_slide()
+	
+	barrel_tip.look_at(mouse_position)
+	
+	if movement_vector == Vector2(-1, 0) or movement_vector == Vector2(1, 0):
+		tank_body.rotation = 90
+	if movement_vector == Vector2(0, 1) or movement_vector == Vector2(0, -1):
+		tank_body.rotation = 0
+	if movement_vector == Vector2(-1, 1) or movement_vector == Vector2(1, -1):
+		tank_body.rotation = 45
+		
 	
 	#var move_sign = sign(movement_vector.x)
 	#if move_sign != 0:
