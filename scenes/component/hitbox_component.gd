d2dcf0932065823b352b70f3a166d69a2bf4bf81
Stephen <stephen.adams@redhat.com>
2023-12-15 17:32:15 -0500

    add projectiles

--- scenes/component/hitbox_component.gd
+++ scenes/component/hitbox_component.gd
@@ -1,4 +1,20 @@
 extends Area2D
 class_name HitboxComponent
 
-var damage = 0
+@export var speed : int = 500
+@export var damage : int = 10
+@export var max_range : int = 150 # need to just remove the projectiles when they go off camera
+@export var fire_delay : float = 0.50
+@export var direction : Vector2 = Vector2.RIGHT
+
+@onready var ammo_sprite : Sprite2D = $Sprite2D
+
+
+	
+func _physics_process(delta):
+	position += transform.x * speed * delta
+	
+func _on_StandardAmmo_body_entered(body):
+	if body.is_in_group("player"):
+		body.queue_free()
+	queue_free()
