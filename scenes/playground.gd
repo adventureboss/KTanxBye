2cf2d0ce72b4de409e290402edbec375891dc3df
justinorringer <justinorringer@gmail.com>
2023-12-16 15:40:23 -0500

    Moved spawns; added playground

---
+++ scenes/playground.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@export var PlayerScene : PackedScene
+
+var players = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# faking some characters
+	players = [{
+		"id": 1,
+		"name": "History",
+		"color": "green"
+	},
+	{
+		"id": 1398740987,
+		"name": "Peerless",
+		"color": "camo"
+	}]
+	var i = 0
+	for p in players:
+		var current_player = PlayerScene.instantiate()
+		current_player.name = str(p.id)
+		add_child(current_player)
+		for spawn in get_tree().get_nodes_in_group("PlayerSpawnPoints"):
+			if spawn.name == str(i):
+				current_player.global_position = spawn.global_position
+		i += 1
